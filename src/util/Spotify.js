1f68c92014f3b290d8fd687604399456d6ad7a70
Shaundai Person <shaundaiperson@gmail.com>
2019-11-09 20:32:16 -0500

    add Spotify json

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,6 @@
 let accessToken;
+const clientId = '818b8b13fa6446e8a347ff143e9e3e88';
+const redirectUri = 'http://localhost:3000/';
 
 const Spotify = {
     getAccessToken(){
@@ -14,7 +16,21 @@ const Spotify = {
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
             return accessToken;
-        }
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+            window.location = accessUrl;
+    }
+    }
+    
+    search(term){
+        const accessToken = Spotify.getAccessToken;
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+          }).then(response =>{
+              if (response.ok){
+                  return response.json();
+              }
+          })
     }
 }
 
